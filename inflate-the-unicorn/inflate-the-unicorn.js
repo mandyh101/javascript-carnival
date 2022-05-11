f3cb16dea09df2f015aee14e8d128e1b1b50fbbb
mandy <mandyhale10@gmail.com>
2022-05-12 11:27:47 +1200

    Add function and variables

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,15 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+//1. Write a function using the onclick method inflateTheUnicorn
+
+let unicorn = document.getElementsByClassName('inflate-an-image')
+
+unicorn.addEventListener('click', inflateTheUnicorn)
+
+function inflateTheUnicorn() {
+  console.log('hello')
+}
+
+console.log('Inflate The Unicorn!')
