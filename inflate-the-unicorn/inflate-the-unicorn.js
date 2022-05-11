a26b8ce7bfb02701008edc4ee5fa3559439102db
mandy <mandyhale10@gmail.com>
2022-05-12 11:40:59 +1200

    Add pseudocode

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,13 +3,12 @@
 // -    -   -   -   -  //
 
 //1. Write a function using the onclick method inflateTheUnicorn
-
-let unicorn = document.getElementsByClassName('inflate-an-image')
-
-unicorn.addEventListener('click', inflateTheUnicorn)
+//ARRAY: the function needs to cycle through the images when you click a unicorn so we need an array
+//LOGIC: we need to make a for loop that uses the click method to execute  function when n imge is clicked
+//FUNCTION: the function will need ti get the HTMl src from each image aand chnage it to the more inflated image
+//ALERT: when a unicorn is fully inflated we need to trigger an alert to thaank the user
 
 function inflateTheUnicorn() {
   console.log('hello')
 }
-
-console.log('Inflate The Unicorn!')
+inflateTheUnicorn()
