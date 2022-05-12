ce8753db823f234c8beeadc872db1c92f90d23b0
mandy <mandyhale10@gmail.com>
2022-05-12 16:55:43 +1200

    tidy up pseudocode

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -22,14 +22,12 @@ let unicornIsInflated = false //i'm going to create a bool variable here as a de
 function inflateTheUnicorn(e) {
   //we need to write a function that will change the img of the unicorn on each click
   //there are three images to change between in order
-  //we need to be able to see what unicorn image we're clicking on
-  //we need to be able to track the inflation level
   //when we reach the image with a fully inflated horn an alert must trigger saying thank you.
-
+  //we need to be able to see what unicorn image we're clicking on
   //The first step is to get the function to change any clicked image src to unicorn-1.png
   let unicorn = e.target //here I have created a vaariable for the specific clicked target
   console.log('this is the ' + unicorn.src)
-  let id = unicorn.id[3]
+  let id = unicorn.id[3] //I got this code from Josephs video. it solved my issue of trying to return the number of the inflated unicorn image in my alert. I created an ID for each unicorn image. This code creates a variable that is the 3rd leter of the elements ID. In this case, the number in the ID I gave these imgs.
   console.log(id)
   if (
     unicorn.src ===
