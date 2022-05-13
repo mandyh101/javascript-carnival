e3cf664b46a7bb1f026e0934293a049eb7802247
mandy <mandyhale10@gmail.com>
2022-05-13 13:26:47 +1200

    Add pseudocode and test array

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,14 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+//PART 1 : store references to all of the cells
+//the best way to do this is to create a 2D array.
+//to create a 2d array we need an array first using the td element
+
+let table = getElementsByTagName('td')
+console.log(table)
+
+//PART 2: Create a function to randomly select a cell
+
+//PART 3: A way to show the mole in a random cell when it is clicked
