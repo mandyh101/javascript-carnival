6289ea5ff9392e310fdced790b12457a80f9d86a
mandy <mandyhale10@gmail.com>
2022-05-13 13:50:25 +1200

    change variable name

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -10,7 +10,8 @@ let tableCells = document.getElementsByTagName('td')
 console.log(tableCells)
 
 //PART 2: Create a function to randomly select a cell
-//Instead of a function will use the javascript random method to assign a random cell using it's index to a variable
-let randomCell = Math.floor(Math.random() * tableCells.length)
+//Instead of a full function ( I was getting a NAN error) will use the javascript random method to assign a random cell using it's index to a variable
+let randomIndex = Math.floor(Math.random() * tableCells.length) //instead of times a number, times length of array and then the code won't break if table size changes
+console.log(randomIndex)
 
 //PART 3: A way to show the mole in a random cell when it is clicked
