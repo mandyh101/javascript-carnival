7ec7e20432cf9da54c5a813cbb2ca420164fb23c
mandy <mandyhale10@gmail.com>
2022-05-13 13:39:21 +1200

    create random number function

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -6,9 +6,14 @@
 //the best way to do this is to create a 2D array.
 //to create a 2d array we need an array first using the td element
 
-let table = getElementsByTagName('td')
-console.log(table)
+let tableCells = document.getElementsByTagName('td')
+console.log(tableCells)
 
 //PART 2: Create a function to randomly select a cell
+
+function getRandomCell(min, max) {
+  console.log(Math.floor(Math.random() * (max - min + 1) + min)) //will change to return when working
+}
+getRandomCell(0, 10) //test arguments
 
 //PART 3: A way to show the mole in a random cell when it is clicked
