78da401943feccd58284a898658168219afe6772
mandy <mandyhale10@gmail.com>
2022-05-15 11:57:58 +1200

    Add pseudocode

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,9 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+//TASK - write some functions to move between the different parts of the clown nd chnge the clowns outfit.
+//BIND the function to arrow keys
+//Left and right = cycle between different clothes
+//up and down to change the bodypart
 
-console.log("Dress The Clown!")
+//The Head
