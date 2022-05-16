c0d8799db32ff5638859aab1a9c226161d7ebfb2
mandy <mandyhale10@gmail.com>
2022-05-16 13:04:51 +1200

    tidy up code

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -7,26 +7,16 @@
 //up and down to change the bodypart
 //Get one bodypart working first, then work on the others, then refactor.
 
-//The Head
-//give the head img an id to reference and assign to a variable
-//create changeClownHead function to chnage the src image of your file to head1
-//test
-//create a variable to store the current number of your head image (outside of function)
-//create a variable to contain your src string so you cana manipulate this with the image number variable
-//use the string variable to update your image src and index one-by-one
-//test
-//Bind the function to your right arrow key to cycle through images
-//add a check to the function so that if the headIndex variable equals greater than 6 to staart back at 0
-
 let clownHead = document.getElementById('head')
 let clownBody = document.getElementById('body')
 let clownFeet = document.getElementById('feet')
-let headIndex = 0
-let bodyIndex = 0
-let feetIndex = 0
 let headSrcStr = './images/head'
 let bodySrcStr = './images/body'
 let feetSrcStr = './images/shoes'
+let headIndex = 0
+let bodyIndex = 0
+let feetIndex = 0
+let bodyPartsIndex = 0
 
 function changeOutfit(change) {
   //my functions were too body part specific. I watched the demo video and learned about using an event paramenter and += to incrementally increase the number variable. I then copied my code to update the source.
@@ -65,32 +55,6 @@ function changeOutfit(change) {
   }
 }
 
-window.addEventListener('keydown', toggleImages, false)
-
-function toggleImages(keyPressed) {
-  if (keyPressed.keyCode == 39) {
-    changeOutfit(1)
-    console.log('right arrow = increase image src index')
-  } else if (keyPressed.keyCode == 37) {
-    changeOutfit(-1)
-    console.log('left arrow = decrease image src index')
-  } else if (keyPressed.keyCode == 38) {
-    console.log('up arrow = change bodyPartIndex up')
-    changeBodyParts(1)
-    console.log(bodyPartsIndex)
-  } else if (keyPressed.keyCode == 40) {
-    console.log('down arrow = change bodyPartIndex down')
-    changeBodyParts(1)
-    console.log(bodyPartsIndex)
-  }
-}
-//Toggle between different body parts
-//create another index to keep track of which body part you are selecting clothing for (array using get element)
-//Write  function that allows you to use the up and down arrows to increase/decrease the clothingindex variable
-//add a check to make sure it doesn't go too high or low
-//console.log to test
-let bodyPartsIndex = 0
-
 function changeBodyParts(change) {
   bodyPartsIndex += change
   if (bodyPartsIndex < 0) {
@@ -100,8 +64,17 @@ function changeBodyParts(change) {
     bodyPartsIndex = 0
   }
 }
-// console.log(bodyPartsIndex)
 
-//Refactor and change all body parts
-//use our head function
-//Our function should cycle through the images depending on what the clothingIndex variabel is set at e.g. if == 0, change head images. If == 1, change body images. If == 2, change shows.
+window.addEventListener('keydown', toggleImages, false)
+
+function toggleImages(keyPressed) {
+  if (keyPressed.keyCode == 39) {
+    changeOutfit(1)
+  } else if (keyPressed.keyCode == 37) {
+    changeOutfit(-1)
+  } else if (keyPressed.keyCode == 38) {
+    changeBodyParts(1)
+  } else if (keyPressed.keyCode == 40) {
+    changeBodyParts(1)
+  }
+}
